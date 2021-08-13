3c5655d5832b2be76463ea432fa5f37bcadc3f51
Mitali-laroia <04042001mitali@gmail.com>
2021-08-13 12:44:26 +0530

    signin route and hashing passwords

--- server/router/auth.js
+++ server/router/auth.js
@@ -9,24 +9,49 @@ router.get('/', (req, res) => {
     res.send(`Hello world from the server rotuer js`);
 });
 
-router.post('/register', (req, res) => {
+router.post('/register', async(req, res) => {
     const {name,email,phone,work,password,cpassword} = req.body;
 
     if(!name || !email || !phone || !work || !password || !cpassword){
         return res.status(442).json({error : "Please fill all the required fields correctly"});
     }
 
-    User.findOne({email:email})
-        .then((userExist) => {
+    try {
+        const userExist = await User.findOne({email:email})
             if(userExist) {
                 return res.status(442).json({error : "Email already exists"});
-            }
-
+            } else if(password != cpassword) {
+                return res.status(422).json({error: "password didn't match"});
+            } else {
             const user = new User({name,email,phone,work,password,cpassword});
-            user.save().then(() => {
-                 res.status(201).json({message : "user registered successfully"});
-            }).catch((err) => res.status(500).json({error : "Registeration falied"}));
-        }).catch(err => {console.log(err)})
+            await user.save();
+            res.status(201).json({message : "user registered successfully"});
+        }
+        } catch (error) {
+        console.log(error);
+    }
 });
+
+
+router.post("/signin", async(req,res)=> {
+    try {
+        const {email,password} = req.body;
+
+        if(!email||!password) {
+            return res.status(400).json({error : "Please fill all the required fiels"});
+        }
+
+        const userLogin = await User.findOne({email:email});
+
+        if(userLogin) {
+
+        } else {
+            res.status(400).json({error : "Invalid credentials"});
+        }
+    } catch (error) {
+        console.log(error);
+    }
+});
+
 
 module.exports = router;
